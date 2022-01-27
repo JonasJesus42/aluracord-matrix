c46c6474a309911b7fa2fafcf9e93b8ce14f7e26
JonasJesus <jonasdasilvajesus@outlook.com>
2022-01-27 20:07:12 -0300

    segundo dia finalizado com o desafio resolvido

---
+++ pages/chat.js
@@ -0,0 +1,7 @@
+export default function PaginaChat(){
+    return(
+        <div>
+            <h1>test</h1>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,8 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 import appConfig from '../config.json';
+import React from 'react'
+import {useRouter} from 'next/router'
 
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -60,11 +34,13 @@ function Titulo(props) {
 // export default HomePage
 
 export default function PaginaInicial() {
-  const username = 'jonasjesus42';
+ /*  const username = 'jonasjesus42'; */
+  const [username, setUsername] = React.useState('jonasjesus42')
+  const roteamento = useRouter()
+  const caracteresNoUsername = username.length
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -91,6 +67,10 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={(infos) => { console.log('test')
+            infos.preventDefault()
+            roteamento.push('/chat')
+          }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -101,7 +81,18 @@ export default function PaginaInicial() {
               {appConfig.name}
             </Text>
 
+            {/* <input type='text' value={username} onChange={(e) =>{ 
+              const valor = e.target.value 
+              setUsername(valor)}}>
+            </input> */}
+
             <TextField
+              value={username}
+              onChange={(e) =>{ 
+                const valor = e.target.value 
+                setUsername(valor) 
+                  }
+                }
               fullWidth
               textFieldColors={{
                 neutral: {
@@ -110,7 +101,7 @@ export default function PaginaInicial() {
                   mainColorHighlight: appConfig.theme.colors.primary[500],
                   backgroundColor: appConfig.theme.colors.neutrals[800],
                 },
-              }}
+              }} 
             />
             <Button
               type='submit'
@@ -148,7 +139,7 @@ export default function PaginaInicial() {
                 borderRadius: '50%',
                 marginBottom: '16px',
               }}
-              src={`https://github.com/${username}.png`}
+               src={`https://github.com/${caracteresNoUsername < 3? null: username}.png`} 
             />
             <Text
               variant="body4"
